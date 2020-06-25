998a0d4eb91ef11c7242715d272fdb610a048109
AVZ <alex.zelenskiy@yahoo.com>
2020-06-25 13:42:59 +0300

    I think - it's done.

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,23 @@
 import './scss/main.scss';
 
+const clear = () => {
+	if (document.mail_form.classList.contains('Errors')) {
+		document.mail_form.classList.remove('Errors');
+	}
+};
+function formSubmit(event) {
+	
+	event.preventDefault();
+	let mail_check_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ,
+		mail = document.mail_form.email_input;
+		if (mail.value.match(mail_check_format)) {
+			mail.value = '';
+			clear();
+		} else {
+			document.mail_form.classList.add('Errors');
+			mail.focus();
+		}
+};
 
-console.log('Scripts file is work now')
+document.mail_form.addEventListener('submit', formSubmit );
+
